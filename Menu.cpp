127cab33c085fcfc8c9b18370b1f92c3beb8525d
David <davidarosu@gmail.com>
2025-09-12 21:28:48 -0300

    logged-in-and-created user-logic

--- Menu.cpp
+++ Menu.cpp
@@ -1,20 +1,58 @@
 #include "Menu.h"
+#include <iostream>
+using namespace std;
 
 Menu::Menu() {
     this->userName = new string("");
     this->password = new string("");
     this->userLogged = new bool(false);
+    this->head = nullptr;
+    createAdminUser();
 }
 
 Menu::~Menu() {
     delete this->userName;
     delete this->password;
     delete this->userLogged;
+
+    User* temp = head;
+    while (temp) {
+        User* next = temp->getNext();
+        delete temp;
+        temp = next;
+    }
+
     cout << "Menu destroyed" << endl;
 }
 
-bool Menu::verifyUserAccount(string* userName, string* password) {
-    return true;
+void Menu::createAdminUser() {
+    addUser("admin", "123");
+}
+
+void Menu::addUser(string userName, string password) {
+    User* newUser = new User(userName, password);
+    if (!head) {
+        head = newUser;
+    } else {
+        User* temp = head;
+        while (temp->getNext()) temp = temp->getNext();
+        temp->setNext(newUser);
+    }
+}
+
+bool Menu::verifyUserAccount(string* userName, string* password, bool creatingNewUser) {
+    User* temp = head;
+    while (temp) {
+        if (temp->getUsername() == *userName) {
+            if (creatingNewUser) {
+                return false;
+            } else {
+                return temp->getPassword() == *password;
+            }
+        }
+        temp = temp->getNext();
+    }
+    return creatingNewUser ? true : false;
 }
 
 void Menu::mainMenu() {
@@ -46,13 +84,13 @@ void Menu::logInMenu() {
     cout << "Enter password: ";
     cin >> *(this->password);
 
-    bool userExist = verifyUserAccount(this->userName, this->password);
+    bool userExist = verifyUserAccount(this->userName, this->password, false);
 
     if (userExist) {
         cout << "Welcome, " << *(this->userName) << "!!" << endl;
         *(this->userLogged) = true;
     } else {
-        cout << "The user does not exist..." << endl;
+        cout << "The user does not exist or password is incorrect..." << endl;
     }
 }
 
@@ -68,10 +106,13 @@ void Menu::signUpMenu() {
     cout << "Repeat password: ";
     cin >> confirmPassword;
 
-    bool userExist = false;
+    bool canCreate = verifyUserAccount(&newUser, &newPassword, true);
 
-    if (!userExist) {
+    if (!canCreate) {
+        cout << "The user already exists!!" << endl;
+    } else {
         if (newPassword == confirmPassword) {
+            addUser(newUser, newPassword);
             *(this->userName) = newUser;
             *(this->password) = newPassword;
             *(this->userLogged) = true;
@@ -79,7 +120,5 @@ void Menu::signUpMenu() {
         } else {
             cout << "Passwords are not the same!!" << endl;
         }
-    } else {
-        cout << "The user already exists!!" << endl;
     }
 }
--- Menu.h
+++ Menu.h
@@ -2,33 +2,37 @@
 #define MENU_H
 
 #include <iostream>
+#include "User.h"
 using namespace std;
 
 class Menu {
 private:
+    User* head;
     string* userName;
     string* password;
     bool* userLogged;
-
-    bool verifyUserAccount(string* userName, string* password);
-
+    
 public:
     Menu();
 
     string getUserName() const { return *(this->userName); }
     string getPassword() const { return *(this->password); }
+
     bool isUserLogged() const { return *(this->userLogged); }
+    bool verifyUserAccount(string* userName, string* password, bool creatingNewUser);
 
     void setUserName(string _userName) { *(this->userName) = _userName; }
     void setPassword(string _password) { *(this->password) = _password; }
     void setUserLogged(bool _logged) { *(this->userLogged) = _logged; }
+
+    void createAdminUser();
+    void addUser(string userName, string password);
 
     void mainMenu();
     void logInMenu();
     void signUpMenu();
     void userInterfaceMenu();
 
-    // Destructor
     ~Menu();
 };
 #endif
